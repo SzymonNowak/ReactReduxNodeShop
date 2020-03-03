679ad11b3bbb2a68ba0a30cec1d7ab4e13c1f8db
SzymonNowak <nnja5@o2.pl>
2020-03-03 14:20:41 +0100

    main user template refactor

--- src/Root.js
+++ src/Root.js
@@ -2,19 +2,25 @@ import React, { Component } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { routes } from "./routes/index";
 import Main from "./views/Main";
-import MainTemplate from "./templates/MainTemplate";
+import MainUserTemaplte from "./templates/MainTemplate";
 import { Provider } from 'react-redux';
 import store from './store/index';
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from './theme/GlobalStyle';
+import { theme } from './theme/mainTheme';
 const Root = () => (
   <Provider store={store}>
-    <BrowserRouter>
-      <MainTemplate>
-        <Switch>
-          {/* <Route exact path={routes.home} render={() => <Redirect to="/" />} /> */}
-          <Route exact path={routes.main} component={Main} />
-        </Switch>
-      </MainTemplate>
-    </BrowserRouter>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <MainUserTemaplte>
+          <Switch>
+            {/* <Route exact path={routes.home} render={() => <Redirect to="/" />} /> */}
+            <Route exact path={routes.main} component={Main} />
+          </Switch>
+        </MainUserTemaplte>
+      </BrowserRouter>
+    </ThemeProvider>
   </Provider>
 );
 
--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,24 +1,15 @@
 import React, { Component } from "react";
-import GlobalStyle from "../theme/GlobalStyle";
 import NavBar from "../components/organism/NavBar/NavBar";
-import { ThemeProvider } from "styled-components";
-import { theme } from "../theme/mainTheme";
+import PropTypes from "prop-types";
 
-class MainTemaplte extends Component {
-  state = {};
 
-  render() {
-    const { children } = this.props;
-    return (
-      <div>
-        <GlobalStyle />
-        <ThemeProvider theme={theme}>
+const MainUserTemaplte = ({children}) => {
+  return(
+    <>
           <NavBar />
           {children}
-        </ThemeProvider>
-      </div>
-    );
-  }
+      </>
+   ) 
 }
 
-export default MainTemaplte;
+export default MainUserTemaplte;
