54a49074a742c622c6a651c68bc061ff8d010089
SzymonNowak <nnja5@o2.pl>
2020-03-23 22:00:29 +0100

    connect back-front work

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser')
+var cors = require('cors');
 const path = require('path');
 const app = express();
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
-
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 
-
+app.get('/', (req, res) => {
+    res.json({
+        id: 'elo'
+    })
+})
 
 app.listen(process.env.PORT || 8080);
