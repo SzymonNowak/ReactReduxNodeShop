522ab9c2a6090c927e8ed7bbe330e8037bc8c547
SzymonNowak <nnja5@o2.pl>
2020-04-09 23:08:26 +0200

    i had to change version of reduxFireStore and reactReduxFirebase

--- Burgermania/ClientApp/src/store/index.js
+++ Burgermania/ClientApp/src/store/index.js
@@ -1,22 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import shopApp from '../reducers/index';
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'
 import { reduxFirestore, getFirestore } from 'redux-firestore';
 import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 import firebase from '../services/firebase';
+import { firebaseConfig } from '../configuration/firebaseConfig'
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
-// const store = createStore(
-//   shopApp /* preloadedState, */,
-//   composeEnhancer(applyMiddleware(thunk.withExtraArgument(getFirestore, getFirebase))),
-
-// );
 const store = createStore(
   shopApp,
   compose(
     composeEnhancer(applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))),
-    reduxFirestore(firebase),
-    reactReduxFirebase(firebase),
+    reactReduxFirebase(firebase), // redux binding for firebase
+    reduxFirestore(firebase) // redux bindings for firestore
   )
 
 );
