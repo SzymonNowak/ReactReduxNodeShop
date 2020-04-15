befbe2d0deed9e48d25c5d3349ed01ba280f396d
SzymonNowak <nnja5@o2.pl>
2020-04-15 20:16:36 +0200

    c# deleted, only firebase

--- Burgermania.Repository/Component.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Burgermania.Repository
-{
-    public class Component
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Price { get; set; }
-    }
-}
