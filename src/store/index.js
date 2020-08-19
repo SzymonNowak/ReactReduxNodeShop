385fa388bc04e28c0c2eb1a3c234686b96a8c8ab
SzymonNowak <nnja5@o2.pl>
2020-08-19 22:17:48 +0200

    before firebase

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,21 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import shopApp from '../reducers/index';
-import thunk from 'redux-thunk'
-import { reduxFirestore, getFirestore } from 'redux-firestore';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
-import firebase from '../services/firebase';
-import { firebaseConfig } from '../configuration/firebaseConfig'
+import { createStore, applyMiddleware, compose } from "redux";
+import shopApp from "../reducers/index";
+import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import firebase from "../services/firebase";
+import { firebaseConfig } from "../configuration/firebaseConfig";
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
 
 const store = createStore(
   shopApp,
   compose(
-    composeEnhancer(applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))),
+    composeEnhancer(
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }))
+    ),
     reactReduxFirebase(firebase), // redux binding for firebase
     reduxFirestore(firebase) // redux bindings for firestore
   )
-
 );
 
 export default store;
--- src/views/Main.js
+++ src/views/Main.js
@@ -1,29 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import Card from "../components/molecules/Card/Card";
 
 const Main = ({ products }) => {
   return (
     <div>
-      {products.map(
-        ({
-          id,
-          productName,
-          productPrice,
-          ingredients,
-          sauce,
-          mealOfTheWeek,
-        }) => (
-          <Card
-            id={id}
-            tittle={productName}
-            price={productPrice}
-            key={productName}
-            ingredients={ingredients}
-            sauce={sauce}
-            mealOfTheWeek={mealOfTheWeek}
-          />
+      {products.length ? (
+        products.map(
+          ({
+            productName,
+            productPrice,
+            ingredients,
+            sauce,
+            mealOfTheWeek,
+          }) => (
+            <Card
+              tittle={productName}
+              price={productPrice}
+              key={productName}
+              ingredients={ingredients}
+              sauce={sauce}
+              mealOfTheWeek={mealOfTheWeek}
+            />
+          )
         )
+      ) : (
+        <p>there is nothing</p>
       )}
     </div>
   );
